646847edf2b85137c7c5a3b270b32eef909fc9f9
IevaMo <mockute1@gmail.com>
2020-12-21 15:21:57 +0100

    Add project files.

---
+++ ConsoleApp.API/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.API
+{
+   public class Post
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+    }
+}
---
+++ ConsoleApp.API/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.API
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+           var httpClient = new HttpClient();
+
+            var response =  await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users/6");
+
+               response.EnsureSuccessStatusCode(); // patikrinimo eilute
+
+               var responseBody = await response.Content.ReadAsStringAsync(); // visur kur bus Async reikes prideti await
+
+               var responseObject = JsonConvert.DeserializeObject<Post>(responseBody);
+
+
+                var responseTwo = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts?userId=6");
+
+                var responseTwoBody = await responseTwo.Content.ReadAsStringAsync();
+
+                var responseTwoObject = JsonConvert.DeserializeObject<List<Get>>(responseTwoBody); // nurodoma, kad isspausdintu sarasa
+
+        }
+    }
+}
